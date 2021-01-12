c75c48c1611c5a8f84b1d3aad718c1047683a123
hamaguchi-tescom <y-hamaguchi@tescomsound.com>
2021-01-12 17:24:06 +0900

    event修正

--- js/index.js
+++ js/index.js
@@ -5,9 +5,10 @@ $(function () {
     };
 
     var timerMs = 1000 * 60 * 10;
+    // var timerMs = 1000 * 10;
     var timerId = setTimeout(toRedirect, timerMs);
 
-    $('div').on('click keypress scroll wheel mousewheel mouseover mouseleave mouseup touchstart touchend touchcancel touchmove', function () {
+    $('div').on('click keypress scroll touchstart touchend touchcancel touchmove', function () {
         clearTimeout(timerId);
         timerId = setTimeout(toRedirect, timerMs);
     });
